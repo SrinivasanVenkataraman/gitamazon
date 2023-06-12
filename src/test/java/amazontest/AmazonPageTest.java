4a4c0ff4ffdf0d27756d2a4c24eb9016914c86fc
srinivasanche86 <srinivasanche86@gmail.com>
2023-06-12 15:04:40 +0530

    DependencyJenadded

--- src/test/java/amazontest/AmazonPageTest.java
+++ src/test/java/amazontest/AmazonPageTest.java
@@ -1,10 +1,30 @@
 package amazontest;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class AmazonPageTest {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+@Test
+	public void testGoogle() throws IOException {
+			WebDriverManager.chromedriver().setup();
+			WebDriver wd=new ChromeDriver();
+			wd.get("https://www.amazon.in/");	
+			File src= ((TakesScreenshot)wd).getScreenshotAs(OutputType.FILE);
+			FileUtils.copyFile(src, new File("screenshot/amazon.png"));
+		
 	}
+	
 
 }
